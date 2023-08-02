c18d4f3b16eb0b72989f661062be5c77acabcf2a
Zarfaa <zarfamasood924@gmail.com>
2023-08-02 05:18:48 +0500

    Changes

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -8,7 +8,7 @@ let NavBar = () => {
   <div className="nav_container">
 
   <div>
-  <Link  to="/" className="nav_logo">Trek Buddy</Link>
+  <Link   to="/" className="nav_logo">Trek Buddy</Link>
   </div>
 
   <div className="nav_items">
@@ -20,16 +20,14 @@ let NavBar = () => {
   </div>
   </div>
 
-  <div className="nav_btn">
-
   <div className="dropdown">
-  <Link  className="signup" to="/signup"><i class="fa fa-fw fa-user"></i>SignUP</Link>
+  <button className="dropbtn" id = "text"><i class="fa fa-fw fa-user"></i>Signup </button>
   <div className="dropdown-content">
-    <Link to="/UserSignUP">User</Link>
-    <Link to="/VendorSignUP">Vendor</Link>
+    <Link to="/UserSignUP">Signup as User</Link>
+    <Link to="/VendorSignUP">Signup as Vendor</Link>
   </div>
   </div>
-  </div>
+
   </div>
 </nav>
  )
